c427b9565167d2625c2e5cae716c35ed4b093cde
David <prtnprvtmail@protonmail.com>
2022-08-12 18:57:10 +0200

    Removed manual symbol exports

--- Sub/librum_global.hpp
+++
@@ -1,7 +0,0 @@
-#include <QtCore/QtGlobal>
-
-#if defined(MYSHAREDLIB_LIBRARY)
-#  define LIBRUM_EXPORT Q_DECL_EXPORT
-#else
-#  define LIBRUM_EXPORT Q_DECL_IMPORT
-#endif
--- Sub/something.hpp
+++ Sub/something.hpp
@@ -1,10 +1,9 @@
 #pragma once
-#include "librum_global.hpp"
 #include "QtQmlIntegration/qqmlintegration.h"
 #include <QObject>
 
 
-class LIBRUM_EXPORT Something : public QObject
+class Something : public QObject
 {
     Q_OBJECT
     QML_ELEMENT
