feb4a140a9f231056737dbea0d4d4f03ac386edc
David <prtnprvtmail@protonmail.com>
2022-08-12 19:36:44 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QtQml/qqmlextensionplugin.h>
+// #include "something.hpp"
 
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    // Something s;
 
     QQmlApplicationEngine engine;
     const QUrl url(u"qrc:/main.qml"_qs);
